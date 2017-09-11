7d7157fb52792f1cc42c1e72eb224735fc08e7ff
Alexander <a.nichiporovich@gmail.com>
2017-09-11 09:55:04 +0300

    Добавил min версии файлов.

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ function showOrHideSearchForm() {
   var form = document.getElementById("searchForm");
   form.hidden = !form.hidden;
 }
+
+function setupPriceRangeSlider() {
+  $( "#rangeSlider" ).slider({
+    range: true,
+    min: 0,
+    max: 5000,
+    values: [ 0, 4000 ],
+    slide: function( event, ui ) {
+      $( "#minPrice" ).html("ОТ " + ui.values[ 0 ]);
+      $( "#maxPrice" ).html("ДО " + ui.values[ 1 ]);
+    }
+  });
+  
+  $( "#minPrice" ).html("ОТ " + $( "#rangeSlider" ).slider( "values", 0 ));
+  $( "#maxPrice" ).html("ДО " + $( "#rangeSlider" ).slider( "values", 1 ));
+}
